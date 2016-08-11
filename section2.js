cebb6f7037c9da79e08b94a5dc028316150308dd
nikolasmurdock <nikolasmurdock@gmail.com>
2016-08-11 16:47:26 -0500

    Everything is JQuery and the paragraphs are all where they need to be.

--- section2.js
+++ section2.js
@@ -1,147 +1,149 @@
-var margin = {top: 20, right: 50, bottom: 0, left: 50},
-    w = 1100 - margin.left - margin.right,
-    h = 400 - margin.top - margin.bottom;
-var padding = 20;
-var barPadding = 2;
-var radio = 10;
+var section2 = function() {
+	var margin = {top: 20, right: 50, bottom: 0, left: 50},
+	    w = 1100 - margin.left - margin.right,
+	    h = 400 - margin.top - margin.bottom;
+	var padding = 20;
+	var barPadding = 2;
+	var radio = 10;
 
-var dataset = [
-	["01/01/2016", 50],
-	["02/02/2016", 100],
-	["03/03/2016", 84],
-	["14/05/2016", 31],
-	["04/04/2016", 89],
-	["05/03/2016", 78],
-	["31/04/2016", 99],
-	["05/05/2016", 63],
-	["06/06/2016", 45],
-	["07/07/2016", 120],
-	["08/08/2016", 50],
-	["11/02/2016", 112],
-	["09/09/2016", 62],
-	["10/10/2016", 23],
-	["11/11/2016", 12],
-	["12/12/2016", 34]
-];
+	var dataset = [
+		["01/01/2016", 50],
+		["02/02/2016", 100],
+		["03/03/2016", 84],
+		["14/05/2016", 31],
+		["04/04/2016", 89],
+		["05/03/2016", 78],
+		["31/04/2016", 99],
+		["05/05/2016", 63],
+		["06/06/2016", 45],
+		["07/07/2016", 120],
+		["08/08/2016", 50],
+		["11/02/2016", 112],
+		["09/09/2016", 62],
+		["10/10/2016", 23],
+		["11/11/2016", 12],
+		["12/12/2016", 34]
+	];
 
-var dateParser = d3.timeParse("%d/%m/%Y");
-var tFormat = d3.timeFormat("%d/%m/%y");
+	var dateParser = d3.timeParse("%d/%m/%Y");
+	var tFormat = d3.timeFormat("%d/%m/%y");
 
-var adjData = function(dataset) {
-	data = dataset.map(function(d) {
-		var parDate = dateParser(d[0]);
-		var value = d[1];
-		return {
-			"date": parDate,
-			"value": value
+	var adjData = function(dataset) {
+		data = dataset.map(function(d) {
+			var parDate = dateParser(d[0]);
+			var value = d[1];
+			return {
+				"date": parDate,
+				"value": value
+			}
+		})
+		function sortByDateAscending(a, b) {
+	    // Dates will be cast to numbers automagically:
+	    	return a.date - b.date;
 		}
-	})
-	function sortByDateAscending(a, b) {
-    // Dates will be cast to numbers automagically:
-    	return a.date - b.date;
+
+		data = data.sort(sortByDateAscending);
+		section2(data);
 	}
+	//data = data.sort(sortByDateAscending);
+	var section2 = function(data) {
+		var svg = d3.select("#section2")
+					.append("svg")
+					.attr("width", w + margin.left + margin.right)
+					.attr("height", h + margin.bottom + margin.top)
+					.append("g")
+					.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		/*Domains*/
+		var xDom = d3.extent(data, function(d) {
+			return d.date;
+		})
 
-	data = data.sort(sortByDateAscending);
-	section2(data);
+		var yDom = d3.extent(data, function(d) {
+			return d.value;
+		})
+
+		/*Scales*/
+		var xScale = d3.scaleTime()
+						.domain(xDom)
+						.range([0, w]);
+		var yScale = d3.scaleLinear()
+						.domain(yDom)
+						.range([h-padding,0]);
+		/*Axes*/
+		var xAxis = d3.axisBottom()
+						.scale(xScale)
+						.ticks(10);
+		var yAxis = d3.axisLeft()
+						.scale(yScale)
+						.ticks(5);
+		/*Div Tooltip*/
+		var div = d3.select("body")
+					.append("div")	
+	    			.attr("class", "tooltip")				
+	    			.style("opacity", 0);
+		/*Draw Path*/
+		var openLine = d3.line()
+						.curve(d3.curveLinear)
+						.x(function(d) {
+							return xScale(d.date)+padding;
+						})
+						.y(function(d) {
+							return yScale(d.value);
+						});
+		svg.append("path")
+			.datum(data)
+			.attr("class", "openLine")
+			.attr("d", openLine);
+
+		/*Draw Dots*/
+		svg.selectAll("circle")
+			.data(data)
+			.enter()
+			.append("circle")
+			.attr("class","dot")
+			.attr("cx", function(d) {
+				return (xScale(d.date)+padding);
+			})
+			.attr("cy", function(d) {
+				return yScale(d.value);
+			})
+			.attr("r", 4)
+			.attr("fill", function(d){
+				var rgbR = Math.round(Math.random() * 255);
+				var rgbR = rgbR.toString(16);
+				var rgbG = Math.round(Math.random() * 255);
+				var rgbG = rgbG.toString(16);
+				var rgbB = Math.round(Math.random() * 255);
+				var rgbB = rgbG.toString(16);
+				return "#"+rgbR+rgbG+rgbB;
+			})
+			.on("mouseover", function(d) {		
+	            div.transition()		
+	                .duration(200)		
+	                .style("opacity", .9);		
+	            div	.html(tFormat(d.date) + "<br/>"  + d.value)	
+	                .style("left", (d3.event.pageX) + "px")		
+	                .style("top", (d3.event.pageY - 28) + "px");	
+	        })					
+	        .on("mouseout", function(d) {		
+	            div.transition()		
+	                .duration(500)		
+	                .style("opacity", 0);
+	        });
+		
+		
+		/*Draw Axes*/
+
+		svg.append("g")
+			.attr("class","axis")
+			.attr("transform", "translate("+padding+"," + (h-padding) + ")")
+			.call(xAxis);
+		svg.append("g")
+			.attr("class", "axis")
+			.attr("transform", "translate(" + padding + ",0)")
+			.call(yAxis);
+
+	}
+	adjData(dataset);
 }
-//data = data.sort(sortByDateAscending);
-var section2 = function(data) {
-	var svg = d3.select("#section2")
-				.append("svg")
-				.attr("width", w + margin.left + margin.right)
-				.attr("height", h + margin.bottom + margin.top)
-				.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-	/*Domains*/
-	var xDom = d3.extent(data, function(d) {
-		return d.date;
-	})
-
-	var yDom = d3.extent(data, function(d) {
-		return d.value;
-	})
-
-	/*Scales*/
-	var xScale = d3.scaleTime()
-					.domain(xDom)
-					.range([0, w]);
-	var yScale = d3.scaleLinear()
-					.domain(yDom)
-					.range([h-padding,0]);
-	/*Axes*/
-	var xAxis = d3.axisBottom()
-					.scale(xScale)
-					.ticks(10);
-	var yAxis = d3.axisLeft()
-					.scale(yScale)
-					.ticks(5);
-	/*Div Tooltip*/
-	var div = d3.select("body")
-				.append("div")	
-    			.attr("class", "tooltip")				
-    			.style("opacity", 0);
-	/*Draw Path*/
-	var openLine = d3.line()
-					.curve(d3.curveLinear)
-					.x(function(d) {
-						return xScale(d.date)+padding;
-					})
-					.y(function(d) {
-						return yScale(d.value);
-					});
-	svg.append("path")
-		.datum(data)
-		.attr("class", "openLine")
-		.attr("d", openLine);
-
-	/*Draw Dots*/
-	svg.selectAll("circle")
-		.data(data)
-		.enter()
-		.append("circle")
-		.attr("class","dot")
-		.attr("cx", function(d) {
-			return (xScale(d.date)+padding);
-		})
-		.attr("cy", function(d) {
-			return yScale(d.value);
-		})
-		.attr("r", 4)
-		.attr("fill", function(d){
-			var rgbR = Math.round(Math.random() * 255);
-			var rgbR = rgbR.toString(16);
-			var rgbG = Math.round(Math.random() * 255);
-			var rgbG = rgbG.toString(16);
-			var rgbB = Math.round(Math.random() * 255);
-			var rgbB = rgbG.toString(16);
-			return "#"+rgbR+rgbG+rgbB;
-		})
-		.on("mouseover", function(d) {		
-            div.transition()		
-                .duration(200)		
-                .style("opacity", .9);		
-            div	.html(tFormat(d.date) + "<br/>"  + d.value)	
-                .style("left", (d3.event.pageX) + "px")		
-                .style("top", (d3.event.pageY - 28) + "px");	
-        })					
-        .on("mouseout", function(d) {		
-            div.transition()		
-                .duration(500)		
-                .style("opacity", 0);
-        });
-	
-	
-	/*Draw Axes*/
-
-	svg.append("g")
-		.attr("class","axis")
-		.attr("transform", "translate("+padding+"," + (h-padding) + ")")
-		.call(xAxis);
-	svg.append("g")
-		.attr("class", "axis")
-		.attr("transform", "translate(" + padding + ",0)")
-		.call(yAxis);
-
-}
-
-$(document).ready(adjData(dataset));
+$(document).ready(section2);
